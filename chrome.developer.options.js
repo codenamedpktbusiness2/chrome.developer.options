77b3a4d2afcc895c53d3644bfc1289bd435320d4
Alice King Tai Poon <23336645+codenamedpkt@users.noreply.github.com>
2022-11-20 18:49:49 +0800

    Update chrome.developer.options.js

--- chrome.developer.options.js
+++ chrome.developer.options.js
@@ -1,4 +1,4 @@
-function eruda() {
+function eruda('cloudflare') {
     var script = document.createElement('script');
     script.src = "//cdnjs.cloudflare.com/ajax/libs/eruda/master";
     document.body.appendChild(script);
@@ -7,9 +7,9 @@ function eruda() {
     }
 }();
 
-function eruda() {
+function eruda('jsdeliver') {
     var script = document.createElement('script');
-    script.src = "//cdnjs.cloudflare.com/ajax/libs/eruda/master";
+    script.src = "//cdn.jsdeliver.net/npm/eruda/master";
     document.body.appendChild(script);
     script.onload = function () {
         eruda.init()
